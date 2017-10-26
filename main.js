fca61718f0c574a9e9b615e42fe2f82617b34ee5
wh1tecat_mac <wh1tecat@wh1tecat-mac.local>
2017-10-26 10:26:11 +0900

    complete

--- main.js
+++ main.js
@@ -2,20 +2,16 @@
 
 const fastify = require("fastify")();
 
-const price_hamburger = {
+const price = {
 	101: 100,
 	102: 130,
 	103: 320,
 	104: 320,
-	105: 380
-};
-const price_sidemenu = {
+	105: 380,
 	201: 150,
 	202: 270,
 	203: 320,
-	204: 280
-};
-const price_drink = {
+	204: 280,
 	301: 100,
 	302: 220,
 	303: 250,
@@ -25,6 +21,14 @@ const price_drink = {
 	307: 100,
 	308: 150
 };
+
+
+/* items自動生成 */
+const items = [];
+
+Object.keys(price).forEach((key) => {
+	items.push(key);
+});
 
 fastify.get("/test", (req, res) => {
 	res.send("hello world");
@@ -38,12 +42,24 @@ fastify.post("/api/checkout", (req, res) => {
 			message: "order_not_found"
 		});
 	}
+
 	if (Array.isArray(req.body.order)) {
+		const orderitem = req.body.order;
+		let order_price = 0;
+
+		orderitem.forEach((val, index, array) => {
+			if (!items.includes(val)) {
+				order_price += parseInt(price[val], 10);
+				console.log(parseInt("add:" + price[val], 10));
+			}
+			console.log(order_price);
+		});
 		res.send({
 			ok: true,
-			message: "item_not_found"
+			amount: order_price,
+			items: orderitem
 		});
-	} else{
+	} else {
 		res.send({
 			ok: false,
 			message: "order_not_found"
@@ -51,7 +67,7 @@ fastify.post("/api/checkout", (req, res) => {
 	}
 });
 
-fastify.listen(3000, (err) => {
+fastify.listen(3002, (err) => {
 	if (err) {
 		throw err;
 	}
